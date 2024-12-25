3128e0430ae62438048ec6c25d1fc450098e56d7
okan <okan@posteo.de>
2024-12-25 15:00:58 +0100

    update deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 
+console.log('process.env.DEPLOY_ENV', process.env.DEPLOY_ENV);
 // Determine the base dynamically
 const isGitHubPages = process.env.DEPLOY_ENV === 'github';
 
