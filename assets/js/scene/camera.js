c00537ba650ec319a55a3bc232d44cab055318e5
okan <okan@posteo.de>
2024-12-27 01:58:35 +0100

    update camera controls

--- assets/js/scene/camera.js
+++ assets/js/scene/camera.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 
 export function setupCamera(scene) {
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10000);
   camera.position.set(0, 0, 100);
   
   scene.add(camera);
--- assets/js/scene/controls.js
+++ assets/js/scene/controls.js
@@ -1,12 +1,17 @@
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
+// import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
+import { MapControls } from 'three/examples/jsm/controls/MapControls';
 
 export function setupControls(camera, renderer) {
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true;
-  controls.dampingFactor = 0.05;
-  controls.screenSpacePanning = false;
-  controls.maxDistance = 100000;
-  controls.maxPolarAngle = Math.PI;
+  // const controls = new TrackballControls(camera, renderer.domElement);
+  // const controls = new PointerLockControls(camera, renderer.domElement);
+  const controls = new MapControls(camera, renderer.domElement);
+  // controls.enableDamping = true;
+  // controls.dampingFactor = 0.05;
+  // controls.screenSpacePanning = false;
+  // controls.maxDistance = 100000;
+  // controls.maxPolarAngle = Math.PI;
   
   return controls;
 } 
