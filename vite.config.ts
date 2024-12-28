018c5a0da9c59a7f940cac04b30dc89dbbf551e8
okan <okan@posteo.de>
2024-12-28 23:01:42 +0100

    put the robot in public

--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,7 @@ import { defineConfig } from 'vite';
 console.log('process.env.DEPLOY_ENV', process.env.DEPLOY_ENV);
 // Determine the base dynamically
 const isGitHubPages = process.env.DEPLOY_ENV === 'github';
-const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  base: isGitHubPages ? '/threejs-cartvisit/' : isProduction ? '/' : '/',
-  build: {
-    assetsDir: 'assets',
-    copyPublicDir: true,
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      },
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (!assetInfo.name) return 'assets/[name]-[hash].[ext]';
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-          if (/\.(gltf|bin)$/.test(assetInfo.name)) {
-            return `assets/models/robot/[name].[ext]`;
-          }
-          return `assets/[name]-[hash].[ext]`;
-        }
-      }
-    }
-  },
-  publicDir: 'public'
+  base: isGitHubPages ? '/threejs-cartvisit/' : '/',
 });
