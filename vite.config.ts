a298b7195356eb3d73248c64baa01d77ba891563
okan <okan@posteo.de>
2024-12-28 22:53:19 +0100

    fix robot load on production

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,12 @@ import { defineConfig } from 'vite';
 console.log('process.env.DEPLOY_ENV', process.env.DEPLOY_ENV);
 // Determine the base dynamically
 const isGitHubPages = process.env.DEPLOY_ENV === 'github';
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  base: isGitHubPages ? '/threejs-cartvisit/' : '/',
+  base: isGitHubPages ? '/threejs-cartvisit/' : isProduction ? '/' : '/',
+  build: {
+    assetsDir: 'assets',
+    copyPublicDir: true,
+  }
 });
